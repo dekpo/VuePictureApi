af662514324c3f551c9103b57e059c4158732fd8
Dekpo <dekpo@me.com>
2022-06-15 10:49:43 +0200

    Created a custom view CreateView

---
+++ src/views/CreateView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Login from '@/components/Login.vue'
+</script>
+<template>
+<div class="container-fluid">
+  <h1>YOU CAN CREATE A NEW PICTURE !!!</h1>
+</div>
+</template>
+
+<style>
+</style>
